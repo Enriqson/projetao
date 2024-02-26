2a8844f73c156b9010222c07094f97025061d7a8
Italo Andrade <ifa2@cin.ufpe.br>
2024-02-26 19:24:46 -0300

    Feat: Change in login screen & creation of a new branch

--- mini-atletas/src/app/profile.tsx
+++ mini-atletas/src/app/profile.tsx
@@ -2,6 +2,7 @@ import { Link } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
 
 export default function Page() {
   return (
@@ -18,17 +19,20 @@ function Content() {
       <View className="py-12 md:py-24 lg:py-32 xl:py-48">
         <View className="container px-4 md:px-6">
           <View className="flex flex-col items-center gap-4 text-center">
+          <Card></Card>
             <Text
               role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
+              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
               Meu Perfil
             </Text>
 
             <View className="gap-4">
               <Link
                 suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors
+                 hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none
+                 
+                 disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
                 href="/"
               >
                 Explore
@@ -46,30 +50,18 @@ function Header() {
   return (
     <View style={{ paddingTop: top }}>
       <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
+        <Link className="flex items-center" href="/">
+          <Text style={{ color: "blue", marginRight: 8 }}>&lt;</Text>
         </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
+        <Text className="font-bold text-blue-500">Meu Perfil</Text>
+        <Link
+          className="text-md font-medium hover:underline web:underline-offset-4 text-blue-500"
+          href="/"
+        >
+          S
+        </Link>
       </View>
     </View>
   );
 }
+
