965548d096854dc1e7d65c723c9ebec2624f5dc8
douglasdodo1 <carvalhodouglas95@gmail.com>
2024-02-27 00:22:10 -0300

    feat: cardAtividadesDiarias SEM PROGRESS BAR

--- mini-atletas/src/app/adventureWeek.tsx
+++ mini-atletas/src/app/adventureWeek.tsx
@@ -5,9 +5,8 @@ import Atividades from "../components/adventures/daily"
 
 function Page() {
   return (
-    <div>
-    <h1>Ola mundo</h1>
-    <Atividades atividades={["Xadrez", "Pedalar", "Pular corda"]} day={'2'} color={"bg-yellow-200"}/>
+    <div className="flex justify-center items-center h-full w-full">
+      <Atividades atividades={["Xadrez", "Pedalar", "Pular corda"]} day={'2'} color={"bg-yellow-200"}/>
     </div>
   );
 }
--- mini-atletas/src/components/adventures/daily.tsx
+++ mini-atletas/src/components/adventures/daily.tsx
@@ -2,37 +2,49 @@ import React from 'react';
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import {AlertDialog,AlertDialogAction,AlertDialogCancel,AlertDialogContent,AlertDialogDescription,AlertDialogFooter,AlertDialogHeader,AlertDialogTitle,AlertDialogTrigger,} from "@/components/ui/alert-dialog"
 import { Button } from "@/components/ui/button"
+import { Progress } from "@/components/ui/progress"
+
 
 const Atividades = ({ atividades, day, color }: {atividades: string[], day: string, color:string}) => {
+
   return (
-    <div className="h-[300px] w-[300px]">
-      <Card className='h-[300px] w-[300px]'>
-        <CardHeader><CardTitle className='text-center'>Dia {day}</CardTitle></CardHeader>
-      <img src='../public/Chess-Knight.png' alt="Logo" className="h-8 mr-2" />
-      {atividades.map((atividade, index) => (
-        <div key={index} className="flex justify-between items-center">
-          <p>{atividade}</p>
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button variant="outline" className='rounded-xl'>Iniciar</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                <AlertDialogDescription>
-                  This action cannot be undone. This will permanently delete your
-                  account and remove your data from our servers.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction>Continue</AlertDialogAction>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </div>
-      ))}
-    </Card></div>
+    <div className="flex justify-center w-5/6 h-full">
+      <div className='relative justify-end h-[25%] w-full bg bg-blue-200 mt-32 border-solid border-2 border-blue-600 rounded-xl z-0 mr-2'>
+      </div>
+      <div className='absolute h-[25%] flex justify-end mt-28 w-5/6  '>
+        <Card className='absolute flex flex-col w-[98%] h-[95%] bg bg-white border-solid border-2 rounded-xl border-black z-1'>
+            <CardHeader><CardTitle className='text-center'>Dia {day}</CardTitle></CardHeader>
+            <div className='w-full flex justify-center'><p>PUT BAR</p></div>
+            <div className='flex flex-col  h-full w-full justify-end p-2'>
+              {atividades.map((atividade, index) => (
+                <div key={index} className="flex justify-between items-center">
+                  <p>{atividade}</p>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="outline"  className='rounded-xl h-1/2'>Iniciar</Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                          This action cannot be undone. This will permanently delete your
+                          account and remove your data from our servers.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
+              ))}
+            </div>
+          </Card>
+      </div>
+          
+    </div>
+      
   );
 };
 
