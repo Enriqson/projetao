79fc68ab1555a4a7cc3349a80166dea264f291b5
Tales Cunha <57573120+Tales-Cunha@users.noreply.github.com>
2024-03-07 20:24:16 +0000

    Adicionado os Trophy para cada atividade

--- mini-atletas/src/components/ActivityBlock/index.tsx
+++ mini-atletas/src/components/ActivityBlock/index.tsx
@@ -6,62 +6,67 @@ import { Link } from "expo-router";
 import ACTIVITY_METADATA from '@/utils/activityMetadata';
 
 function ActivityBlock({
-    ActivitySvg,
-    activityName,
-  }) {
-    const activityMetadata = ACTIVITY_METADATA[activityName]
-    const color_primary = TAILWIND_THEME.colors["light_"+activityMetadata["color"]]
-    const color_secondary = TAILWIND_THEME.colors[activityMetadata["color"]]
-    const activityAlias = activityMetadata.alias
+  ActivitySvg,
+  activityName,
+}) {
+  const activityMetadata = ACTIVITY_METADATA[activityName]
+  const color_primary = TAILWIND_THEME.colors["light_" + activityMetadata["color"]]
+  const color_secondary = TAILWIND_THEME.colors[activityMetadata["color"]]
+  const activityAlias = activityMetadata.alias
+  const Trophy = activityMetadata.Trophy
 
-    const textStyle = {
-      color: color_secondary,
-    };
-    return (
-      <Link href={ `/activity_details/${activityName}`} className="my-5">
-        <OffsetBorder
-          color_primary={color_primary}
-          color_secondary={color_secondary}
-        >
-          <View className="bg-white rounded-[32px] border-black border-[2px] flex-row lg:h-[12vh] lg:w-[30vw] h-[15vh] w-[80vw]  justify-around px-4 py-2">
-            <ActivitySvg />
-            <View className="flex-col pl-2">
-              <Text
-                className="text-[20px] text-center font-bold"
-                style={textStyle}
-              >
-                {activityAlias}
-              </Text>
-              <View className="justify-around items-start flex-col text-[14px] font-normal">
-                <View>
-                  <View className="flex-row justify-between">
-                    <View className=" pr-5">
-                      <Text
-                        className="text-[14px] text-left font-normal"
-                        style={textStyle}
-                      >
-                        Meta semanal:
-                      </Text>
-                    </View>
-                    <View>
-                      <Text style={textStyle}>1 Hora</Text>
-                    </View>
+  const textStyle = {
+    color: color_secondary,
+  };
+  return (
+    <Link href={`/activity_details/${activityName}`} className="my-5">
+      <OffsetBorder
+        color_primary={color_primary}
+        color_secondary={color_secondary}
+      >
+        <View className="bg-white rounded-[32px] border-black border-[2px] flex-row lg:h-[12vh] lg:w-[30vw] h-[15vh] w-[80vw] justify-around px-4 py-5">
+          <ActivitySvg />
+          <View className="flex-col pl-2">
+            <Text
+              className="text-[20px] text-center font-bold"
+              style={textStyle}
+            >
+              {activityAlias}
+            </Text>
+            <View className="justify-around items-start flex-col text-[14px] font-normal space-y-5">
+              <View>
+                <View className="flex-row justify-between">
+                  <View className=" pr-5">
+                    <Text
+                      className="text-[14px] text-left font-normal"
+                      style={textStyle}
+                    >
+                      Meta semanal:
+                    </Text>
+                  </View>
+                  <View>
+                    <Text style={textStyle}>1 Hora</Text>
                   </View>
                 </View>
-                <View>
-                  <Text
-                    className="text-[14px] text-left font-normal"
-                    style={textStyle}
-                  >
-                    Conquistas
-                  </Text>
+              </View>
+              <View className="justify-around flex-row space-x-12">
+                <Text
+                  className="text-[14px] text-left font-normal"
+                  style={textStyle}
+                >
+                  Conquistas
+                </Text>
+                <View className='flex-row'>
+                  <Text style={{ color: color_secondary }}>{activityMetadata["achievements"]}</Text>
+                  <Trophy />
                 </View>
               </View>
             </View>
           </View>
-        </OffsetBorder>
-      </Link>
-    );
-  }
+        </View>
+      </OffsetBorder>
+    </Link>
+  );
+}
 
 export default ActivityBlock
