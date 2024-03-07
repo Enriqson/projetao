c883a305cfd942923845baf31ed33418c55c2e53
Enriqson <elaborao@hotmail.com>
2024-03-07 00:08:56 -0300

    add profile screen

--- mini-atletas/src/app/profile.tsx
+++ mini-atletas/src/app/profile.tsx
@@ -1,75 +1,141 @@
-import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Girl2 from "@/components/svgs/profile/girl2";
+import { Text, View, ScrollView } from "react-native";
+import SetaDireita from "@/components/svgs/profile/setadireita";
+import { TouchableOpacity } from "react-native";
+import GrupoFutebol from "@/components/svgs/profile/gruposfutebol";
+import GrupoPique from "@/components/svgs/profile/grupopiqueesconde";
+import { default as AchievementSvg } from "@/components/svgs/misc/achievement";
+import { Link } from "expo-router";
+import { TAILWIND_THEME } from "@/utils/index";
+import OffsetBorder from "@/components/OffsetBorder";
+
+function AchievementSection() {
+  return (
+    <View className="mb-[10px]">
+      <View className="my-[10px] ml-[25px]">
+        <Text className="text-xl text-gray-400 ">Minhas conquistas</Text>
+      </View>
+
+      <OffsetBorder>
+        <View className="bg-white rounded-[32px] border-black border-[2px] flex-row lg:w-[30vw] h-[81px] w-[90vw] justify-around items-center px-4 py-2">
+          <Link href="/achievements">
+            <TouchableOpacity>
+              <AchievementSvg
+                color_primary={TAILWIND_THEME.colors.light_blue}
+                color_secondary={TAILWIND_THEME.colors.blue}
+              ></AchievementSvg>
+            </TouchableOpacity>
+          </Link>
+
+          <Link href="/achievements">
+            <TouchableOpacity>
+              <AchievementSvg
+                color_primary={TAILWIND_THEME.colors.light_yellow}
+                color_secondary={TAILWIND_THEME.colors.yellow}
+              ></AchievementSvg>
+            </TouchableOpacity>
+          </Link>
+
+          <Link href="/achievements">
+            <TouchableOpacity>
+              <AchievementSvg
+                color_primary={TAILWIND_THEME.colors.light_pink}
+                color_secondary={TAILWIND_THEME.colors.pink}
+              ></AchievementSvg>
+            </TouchableOpacity>
+          </Link>
+
+          <Link href="/achievements">
+            <TouchableOpacity>
+              <AchievementSvg
+                color_primary={TAILWIND_THEME.colors.light_green}
+                color_secondary={TAILWIND_THEME.colors.green}
+              ></AchievementSvg>
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </OffsetBorder>
+    </View>
+  );
+}
+
+function GroupSection() {
+  return (
+    <>
+      <View className="my-[10px] ml-[25px]">
+        <Text className="text-xl text-gray-400 flex items-center">
+          Meu grupos
+          <View className="absolute right-0">
+            <TouchableOpacity>
+              <SetaDireita></SetaDireita>
+            </TouchableOpacity>
+          </View>
+        </Text>
+      </View>
+
+      <View className="mb-[10px]">
+        <Text className="flex left-2 items-center">
+          <GrupoFutebol></GrupoFutebol>
+          <GrupoPique className="absolute left-[175px]"></GrupoPique>
+        </Text>
+      </View>
+    </>
+  );
+}
+
+function ActivitySection() {
+  return (
+    <>
+      <View className="mb-[10px]">
+        <Text className="text-lg text-gray-500 md:text-xl dark:text-gray-400 flex items-center">
+          Minhas atividades
+          <View className="ml-auto">
+            <Link href="/activities">
+              <TouchableOpacity>
+                <SetaDireita></SetaDireita>
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </Text>
+      </View>
+    </>
+  );
+}
+
+const ProfileContent = ({ userName }) => {
+  return (
+    <ScrollView className="">
+      <View className="flex flex-col justify-start items-center h-[120vh]">
+        <View>
+          <View>
+            <Girl2></Girl2>
+          </View>
+
+          <AchievementSection></AchievementSection>
+
+          <View className="my-[5px] flex justify-between">
+            <GroupSection></GroupSection>
+            <ActivitySection></ActivitySection>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+
 
 export default function Page() {
   return (
     <View className="flex flex-1">
-      <Header />
-      <Content />
-    </View>
-  );
-}
-
-function Content() {
-  return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="container px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-3xl text-center native:text-5xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Meu Perfil
-            </Text>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
-          </View>
-        </View>
+      <View className="mt-8">
+        <ProfileContent userName="Leticia"></ProfileContent>
       </View>
+
+
     </View>
+    
   );
 }
 
-function Header() {
-  const { top } = useSafeAreaInsets();
-  return (
-    <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
-          ACME
-        </Link>
-        <View className="flex flex-row gap-4 sm:gap-6">
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            About
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Product
-          </Link>
-          <Link
-            className="text-md font-medium hover:underline web:underline-offset-4"
-            href="/"
-          >
-            Pricing
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
-}
