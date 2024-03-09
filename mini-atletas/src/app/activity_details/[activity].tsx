72db7a162bb8fc4c50d9ec3d6716cf1bf5dbd6f0
Leonardo Brahim Tavares <leonardobrahim@gmail.com>
2024-03-09 14:06:34 -0300

    Fixing Start Button and content texts

--- mini-atletas/src/app/activity_details/[activity].tsx
+++ mini-atletas/src/app/activity_details/[activity].tsx
@@ -1,6 +1,4 @@
-import { StyleSheet } from "nativewind";
-import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useLocalSearchParams } from 'expo-router';
 import ACTIVITY_METADATA from '@/utils/activityMetadata';
 import { TAILWIND_THEME } from "@/utils/index";
@@ -12,66 +10,25 @@ export default function Page() {
 
   return (
     <View className="flex flex-1">
-      <ContentWithStartButton activity={activity} />
+      <Content activity={activity} />
     </View>
   );
 }
 
-function StartButton({ onPress }) {
-  const [isPressed, setIsPressed] = useState(false);
+function Content({ activity }) {
+  const activityMetadata = ACTIVITY_METADATA[activity];
+  const color_primary = TAILWIND_THEME.colors["light_" + activityMetadata["color"]];
+  const color_secondary = TAILWIND_THEME.colors[activityMetadata["color"]];
+  const Image = activityMetadata["Image"];
 
-  const handlePressIn = () => {
-    setIsPressed(true);
+  const handlePress = () => {
+    // Lógica a ser executada quando o botão for pressionado
   };
-
-  const handlePressOut = () => {
-    setIsPressed(false);
-  };
-
-  return (
-    <View className="relative flex mt-5"
-    >
-      <View style={{
-        transform: [{ rotate: "-1.57deg" }],
-      }}>
-        <View className="rounded-[100px] lg:h-[5vh] lg:w-[22vw] h-[8vh] w-[60vw] absolute top-[10px]  border-light_blue border-2" />
-        <TouchableOpacity
-          onPress={onPress}
-          onPressIn={handlePressIn}
-          onPressOut={handlePressOut}
-          className="flex-row justify-center items-center bg-white p-[10px] lg:h-[5vh] lg:w-[22vw] h-[8vh] w-[60vw] rounded-[100px] border-black border-2 active:bg-light_blue"
-        >
-          <Text
-            style={[styles.buttonText, isPressed && styles.buttonTextPressed]}
-          >
-            Iniciar
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  buttonText: {
-    fontSize: 20,
-    color: "#545F71",
-  },
-  buttonTextPressed: {
-    color: "#fff", // Altera para a cor do texto desejada quando pressionado
-  },
-});
-
-function ContentWithStartButton({ activity }) {
-  const activityMetadata = ACTIVITY_METADATA[activity]
-  const color_primary = TAILWIND_THEME.colors["light_" + activityMetadata["color"]]
-  const color_secondary = TAILWIND_THEME.colors[activityMetadata["color"]]
-  const Image = activityMetadata["Image"]
 
   return (
     <View className="justify-center items-center">
       <View>
-        <View className="my-[20px] items-center">
+        <View className="mt-[20px] mb-[50px] items-center">
           <OffsetBorder color_primary={color_primary} color_secondary={color_secondary}>
             <View className="border-black border-[2px] rounded-[32px] bg-white lg:h-[50vh] lg:w-[50vw] h-[60vh] w-[80vw] ">
               <View className="flex mx-[20px] my-[20px] ">
@@ -79,7 +36,6 @@ function ContentWithStartButton({ activity }) {
                   fontFamily: "Kanit",
                   fontSize: 48,
                   fontWeight: "bold",
-                  //lineHeight: "28px",
                   color: color_secondary,
                 }}>{activityMetadata["alias"]}</Text>
                 <View className="my-[10px]">
@@ -99,13 +55,56 @@ function ContentWithStartButton({ activity }) {
                   color: color_secondary,
                 }}>O que preciso ?</Text>
                 <Text className="my-[10px]">{activityMetadata["requirements"]}</Text>
+
+
               </View>
             </View>
           </OffsetBorder>
         </View>
       </View>
-      <StartButton onPress={() => { }} />
+      <StartButton onPress={handlePress} activity={activity}>Iniciar</StartButton>
     </View>
+  );
+}
+
+export function StartButton({ onPress, children, activity }) {
+  const activityMetadata = ACTIVITY_METADATA[activity];
+  const color_primary = TAILWIND_THEME.colors["light_" + activityMetadata["color"]];
+  const color_secondary = TAILWIND_THEME.colors[activityMetadata["color"]];
+
+  return (
+    <Pressable onPress={onPress}>
+      {({ pressed }) => (
+        <View>
+          <View className="justify-center items-center border-black border-[2px] rounded-[25px] bg-white w-[251px] h-[50px]"
+            style={{
+              backgroundColor: pressed ? color_secondary : 'white',
+              borderColor: pressed ? color_secondary : 'black',
+              transform: [{ rotate: "-1.57deg" }],
+              zIndex: 1,
+            }}
+          >
+            <Text style={{
+              color: pressed ? '#FFFFFF' : '#545F71',
+              fontFamily: "Kanit",
+              fontWeight: "400",
+              fontSize: 24,
+            }}>
+              {children}
+            </Text>
+          </View>
+          <View className="border-[2px] rounded-[25px] bg-white w-[251px] h-[50px] mt-[-43px]"
+            style={{
+              backgroundColor: pressed ? color_primary : 'white',
+              transform: [{ rotate: "-1.57deg" }],
+              borderColor: color_secondary,
+              zIndex: 0,
+            }}
+          >
+          </View>
+        </View>
+      )}
+    </Pressable>
   );
 }
 
