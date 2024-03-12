15d60c8efdd5ca6d0a11cd2fb4e122dfb6106f54
Leonardo Brahim Tavares <leonardobrahim@gmail.com>
2024-03-11 23:25:32 -0300

    refactoring and removing zindex from cards

--- mini-atletas/src/app/date.tsx
+++ mini-atletas/src/app/date.tsx
@@ -17,9 +17,6 @@ function Content() {
   const [months, setMonths] = useState([]);
 
   useEffect(() => {
-    // fetch('http://localhost:3000/months')
-    //     .then(response => response.json())
-    //     .then(data => setMonths(data));
     const mockData = [
       {
         name: "Abril",
@@ -77,122 +74,12 @@ function Content() {
 
     setMonths(mockData);
   }, []);
+
   return (
     <ScrollView>
-      <View className=" w-4/5 lg:w-2/4 self-center h-[175vh] justify-start items-center ">
+      <View className="w-4/5 lg:w-2/4 self-center h-[175vh] justify-start items-center">
         {months.map((month, index) => (
-          <View key={index} className="items-center my-4 ">
-            <Text
-              style={{
-                fontSize: 24,
-                fontWeight: "500",
-                fontFamily: "Kanit",
-                color: "#545F71",
-              }}
-            >
-              {month.name}
-            </Text>
-            {Object.entries(month.dates).map(
-              (
-                [day, activity]: [
-                  string,
-                  {
-                    activityName: string;
-                    profileImage: string;
-                    profileName: string;
-                    time: number;
-                    activitySvg: string;
-                  }
-                ],
-                i
-              ) => (
-                <View key={i} className="flex-row space-x-3">
-                  <View className="mr-[2px]" style={{ zIndex: 2 }}>
-                    <Block
-                      activityName={activity.activityName}
-                      heigh={146}
-                      width={163}
-                    >
-                      <View>
-                        <Text
-                          style={{
-                            fontSize: 24,
-                            fontWeight: "500",
-                            fontFamily: "Kanit",
-                            color: "#545F71",
-                            marginLeft: 10,
-                          }}
-                        >
-                          Dia {day}
-                        </Text>
-                        <View className="flex-row items-end">
-                          <Girl />
-                          <Trophy />
-                        </View>
-                      </View>
-                    </Block>
-                  </View>
-                  <View style={{ zIndex: 1 }}>
-                    <Block
-                      activityName={activity.activityName}
-                      heigh={146}
-                      width={194}
-                    >
-                      <View className="flex-row ">
-                        <View></View>
-                        <View>
-                          <Text
-                            style={{
-                              fontSize: 16,
-                              fontWeight: "500",
-                              fontFamily: "Hind",
-                              color: "#545F71",
-                            }}
-                          >
-                            {activity.activityName}
-                            {"\n"}com
-                          </Text>
-                          <Text
-                            style={{
-                              fontSize: 24,
-                              fontWeight: "500",
-                              fontFamily: "Kanit",
-                              color: "#8928C5",
-                            }}
-                          >
-                            {activity.profileName}
-                          </Text>
-                          <Text
-                            style={{
-                              fontSize: 12,
-                              fontWeight: "500",
-                              fontFamily: "Hind",
-                              color: "#545F71",
-                            }}
-                          >
-                            {activity.time}
-                          </Text>
-                          <View className="flex-row items-end">
-                            <Trophy />
-                            <Text
-                              style={{
-                                fontSize: 12,
-                                fontWeight: "500",
-                                fontFamily: "Hind",
-                                color: "#545F71",
-                              }}
-                            >
-                              0
-                            </Text>
-                          </View>
-                        </View>
-                      </View>
-                    </Block>
-                  </View>
-                </View>
-              )
-            )}
-          </View>
+          <Month key={index} month={month} />
         ))}
         <View>
           <Block
@@ -209,3 +96,99 @@ function Content() {
     </ScrollView>
   );
 }
+
+function Month({ month }) {
+  return (
+    <View className="items-center my-4">
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "500",
+          color: "#545F71",
+        }}
+      >
+        {month.name}
+      </Text>
+      {Object.entries(month.dates).map(([day, activity], i) => (
+        <Day key={i} day={day} activity={activity} />
+      ))}
+    </View>
+  );
+}
+
+function Day({ day, activity }) {
+  return (
+    <View className="flex-row space-x-3">
+      <View className="mr-[10px]">
+        <Block activityName={activity.activityName} heigh={146} width={163}>
+          <View>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: "500",
+                color: "#545F71",
+                marginLeft: 10,
+              }}
+            >
+              Dia {day}
+            </Text>
+            <View className="flex-row items-end">
+              <Girl />
+              <Trophy />
+            </View>
+          </View>
+        </Block>
+      </View>
+      <View>
+        <Block activityName={activity.activityName} heigh={146} width={194}>
+          <View className="flex-row ">
+            <View></View>
+            <View>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: "500",
+                  color: "#545F71",
+                }}
+              >
+                {activity.activityName}
+                {"\n"}com
+              </Text>
+              <Text
+                style={{
+                  fontSize: 24,
+                  fontWeight: "500",
+                  color: "#8928C5",
+                }}
+              >
+                {activity.profileName}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 12,
+                  fontWeight: "500",
+                  color: "#545F71",
+                }}
+              >
+                {activity.time}
+              </Text>
+              <View className="flex-row items-end">
+                <Trophy />
+                <Text
+                  style={{
+                    fontSize: 12,
+                    fontWeight: "500",
+                    color: "#545F71",
+                  }}
+                >
+                  0
+                </Text>
+              </View>
+            </View>
+          </View>
+        </Block>
+      </View>
+    </View>
+  );
+}
+
