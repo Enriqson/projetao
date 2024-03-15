d7c0a30d6b3f57e9a0d0b43f08916d1b002ef707
Enriqson <elaborao@hotmail.com>
2024-03-15 16:59:56 -0300

    cleaning up profile page

--- mini-atletas/src/app/(app)/profile.tsx
+++ mini-atletas/src/app/(app)/profile.tsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-//import  ProfileContent  from '@/components/profile/ProfileContent'
-import Girl2 from "@/components/svgs/profile/girl2";
 import { Text, View, ScrollView } from "react-native";
 import SetaDireita from "@/components/svgs/profile/setadireita";
 import { TouchableOpacity } from "react-native";
@@ -12,54 +9,49 @@ import { Link } from "expo-router";
 import { TAILWIND_THEME } from "@/utils/index";
 import OffsetBorder from "@/components/OffsetBorder/index";
 import OffsetProfile from "@/components/OffsetBorder/OffsetProfile";
-import Trofeu from "@/components/svgs/profile/Trofeu"
-import Avatar, {
-  AvatarFullConfig,
-  HairStyleType,
-  genConfig,
-} from "@zamplyy/react-native-nice-avatar";
+import Trofeu from "@/components/svgs/profile/Trofeu";
+import Avatar from "@zamplyy/react-native-nice-avatar";
 import ActivityBlock from "@/components/ActivityBlock";
-import Ball from "@/components/svgs/activities/Ball";
-import Rope from "@/components/svgs/activities/Rope";
-import Bike from "@/components/svgs/activities/Bike";
-import Chess from "@/components/svgs/activities/Chess";
-import Hide from "@/components/svgs/activities/Hide";
 import Esporte from "@/components/svgs/profile/Esporte";
 import Lt from "@/components/svgs/profile/Lt";
 import Gt from "@/components/svgs/profile/Gt";
 import { useAuth } from "@/providers/AuthProvider";
 import { supabase } from "@/config/supabase";
 
-
-
 function ProfileParent() {
   return (
     <OffsetBorder>
-    <View className="bg bg-white h-[200px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2 flex-row"> 
-      
-      <View className="-mr-8">
-          <Text className="text-2xl font-bold opacity-65 mb-4">Parear Perfil</Text>
+      <View className="bg bg-white h-[200px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2 flex-row">
+        <View className="-mr-8">
+          <Text className="text-2xl font-bold opacity-65 mb-4">
+            Parear Perfil
+          </Text>
           <View className="bg bg-white h-[40px] rounded-[10px] border-black border-[2px] mb-4">
-            <Text className="flex justify-center items-center h-screen">enviar sms</Text>
+            <Text className="flex justify-center items-center h-screen">
+              enviar sms
+            </Text>
           </View>
 
           <View className="bg bg-white h-[40px] rounded-[10px] border-black border-[2px] ">
-            <Text className="flex justify-center items-center h-screen">enviar por Email</Text>
+            <Text className="flex justify-center items-center h-screen">
+              enviar por Email
+            </Text>
           </View>
-      </View>
+        </View>
 
-      <View>
+        <View>
           <View className="bg bg-white h-[130px] w-[140px] rounded-[10px] border-black border-[2px] mt-3 ml-5">
-            <Text className="flex justify-center items-center h-screen text-xs">QR Code</Text>
+            <Text className="flex justify-center items-center h-screen text-xs">
+              QR Code
+            </Text>
           </View>
+        </View>
       </View>
-
-    </View>
-  </OffsetBorder>
+    </OffsetBorder>
   );
 }
 
-function MyDates ({ userName, dia }) {
+function MyDates({ userName, dia }) {
   const { user } = useAuth();
   const [avatarConfig, setAvatarConfig] = useState(null);
 
@@ -86,41 +78,47 @@ function MyDates ({ userName, dia }) {
   }, [user]);
   return (
     <View className="mb-[10px]">
-    <View className="my-[10px] ml-[25px]">
-      <Text className="text-xl text-gray-600 font-semibold">Minhas Datas</Text>
-    </View>
-    <OffsetBorder>
-    <View className="bg bg-white h-[200px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2"> 
-      <View className="flex-row">
-      <Text className="mt-2"><Lt></Lt></Text>
-      <Text className="text-2xl font-bold opacity-60">Março</Text>
-      <Text className="mt-2 ml-2"><Gt></Gt></Text>
-      
+      <View className="my-[10px] ml-[25px]">
+        <Text className="text-xl text-gray-600 font-semibold">
+          Minhas Datas
+        </Text>
       </View>
+      <OffsetBorder>
+        <View className="bg bg-white h-[200px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2">
+          <View className="flex-row">
+            <Text className="mt-2">
+              <Lt></Lt>
+            </Text>
+            <Text className="text-2xl font-bold opacity-60">Março</Text>
+            <Text className="mt-2 ml-2">
+              <Gt></Gt>
+            </Text>
+          </View>
 
-      <View className="flex-row mb-3">
-        <View className="bg bg-white h-[100px] rounded-[10px] border-black border-[2px] lg:w-[30vw] w-[100px] justify-around items-center px-4 py-2 flex-col mr-1">
-          <View><Esporte></Esporte></View>
-          <View className="text-xl font-bold opacity-60">Futebol</View>
+          <View className="flex-row mb-3">
+            <View className="bg bg-white h-[100px] rounded-[10px] border-black border-[2px] lg:w-[30vw] w-[100px] justify-around items-center px-4 py-2 flex-col mr-1">
+              <View>
+                <Esporte></Esporte>
+              </View>
+              <View className="text-xl font-bold opacity-60">Futebol</View>
+            </View>
+            <View className="bg bg-white h-[100px] rounded-[10px] border-black border-[2px] lg:w-[30vw] w-[250px] justify-around items-center px-4 py-2 flex-row ml-2">
+              <Avatar size={75} {...avatarConfig}></Avatar>
+              <View className="flew-col">
+                <Text className="font-bold text-xl color-purple">
+                  {userName}
+                </Text>
+                <Text className="text-sm font-semibold opacity-60">
+                  Dia {dia}
+                </Text>
+              </View>
+            </View>
+          </View>
         </View>
-        <View className="bg bg-white h-[100px] rounded-[10px] border-black border-[2px] lg:w-[30vw] w-[250px] justify-around items-center px-4 py-2 flex-row ml-2">
-        <Avatar size={75} {...avatarConfig}></Avatar>
-        <View className="flew-col">
-          <Text className="font-bold text-xl color-purple">{userName}</Text>
-          <Text className="text-sm font-semibold opacity-60">Dia {dia}</Text>
-        </View>
-        
-    
-        </View>
-      </View>
-      
-
+      </OffsetBorder>
     </View>
-  </OffsetBorder>
-  </View>
   );
 }
-
 
 function ProfileChield({ userName }) {
   const { user } = useAuth();
@@ -147,35 +145,36 @@ function ProfileChield({ userName }) {
 
     fetchAvatar();
   }, [user]);
-  return(
-    
-    <OffsetProfile>
-    <View className="bg bg-white h-[160px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2 flex-row"> 
-      
-      <View className="-mr-8">
-          <Avatar size={125} {...avatarConfig} ></Avatar>
-      </View>
 
-      <View>
+  console.log(avatarConfig)
+  return (
+    <OffsetProfile>
+      <View className="bg bg-white h-[160px] rounded-[25px] border-black border-[2px] lg:w-[30vw] w-[90vw] justify-around items-center px-4 py-2 flex-row">
+        <View className="-mr-8 border-purple rounded-[50px] border-[6px] p-[1px]">
+          <Avatar size={125} style={{backgroundColor:"transparent"}} {...avatarConfig}></Avatar>
+        </View>
+
+        <View>
           <Text className="font-bold text-xl color-purple">{userName}</Text>
           <Text className="text-sm color-purple">Futebol</Text>
           <View className="flex flex-row items-end">
-           <Trofeu></Trofeu>
-           <Text className="text-sm color-purple">5x</Text>
+            <Trofeu></Trofeu>
+            <Text className="text-sm color-purple">5x</Text>
           </View>
           <Text className="text-sm color-purple">Corredora</Text>
+        </View>
       </View>
-
-    </View>
-  </OffsetProfile>
-  )
+    </OffsetProfile>
+  );
 }
 
 function AchievementSection() {
   return (
     <View className="mb-[10px]">
       <View className="my-[10px] ml-[25px]">
-        <Text className="text-xl text-gray-600 font-semibold">Minhas conquistas</Text>
+        <Text className="text-xl text-gray-600 font-semibold">
+          Minhas conquistas
+        </Text>
       </View>
 
       <OffsetBorder>
@@ -233,155 +232,84 @@ function GroupSection() {
   return (
     <>
       <View className="my-[10px] ml-[25px]">
-        <Text className="text-xl text-gray-600 font-semibold flex items-center">
-        
-          Meus grupos
-          <View className="absolute right-0">
+        <View className="flex-row items-center">
+          <Text className="text-xl text-gray-600 font-semibold">
+            Meus grupos
+          </Text>
+          <View className="ml-auto mr-6">
             <TouchableOpacity>
               <SetaDireita></SetaDireita>
             </TouchableOpacity>
           </View>
-        </Text>
+        </View>
       </View>
 
       <View className="mb-[10px]">
-        <Text className="flex left-2 items-center">
-          <GrupoFutebol></GrupoFutebol>
-          <GrupoPique className="absolute left-[175px]"></GrupoPique>
+        <Text className="flex-row left-2 items-start">
+          <View>
+            <GrupoFutebol />
+          </View>
+          <View>
+            <GrupoPique />
+          </View>
         </Text>
       </View>
     </>
   );
 }
 
-function ActivitySectionChild() {
+function ActivitySection() {
   return (
     <>
-      <View className="my-[10px] ml-[25px]">
-        <Text className="text-xl text-gray-600 font-semibold flex items-center">
-          Minhas atividades
-          <View className="ml-auto">
+      <View className="my-[10px]">
+        <View className="flex-row ml-[25px]">
+          <Text className="text-xl text-gray-600 font-semibold flex items-center">
+            Minhas atividades
+          </Text>
+          <View className="ml-auto mr-6">
             <Link href="/activities">
               <TouchableOpacity>
                 <SetaDireita></SetaDireita>
               </TouchableOpacity>
             </Link>
           </View>
-        </Text>
-        <View className=" w-4/5 lg:w-2/4 self-center h-[120vh] justify-start">
-            <View
-              style={{
-                flexGrow: 1,
-                alignItems: "center",
-                flexDirection: "column",
-                flexShrink: 1,
-              }}
-            >
-              <ActivityBlock
-                activityName="soccer"
-                ActivitySvg={Ball}
-              />
-              <ActivityBlock
-                activityName="rope_jumping"
-                ActivitySvg={Rope}
-              />
-              <ActivityBlock
-                activityName="cycling"
-                ActivitySvg={Bike}
-              />
-              <ActivityBlock
-                activityName="chess"
-                ActivitySvg={Chess}
-              />
-              <ActivityBlock
-                activityName="hide_and_seek"
-                ActivitySvg={Hide}
-              />
-            </View>
-          </View>
+        </View>
+        <View className="justify-center items-center">
+          <ActivityBlock activityName="soccer" />
+          <ActivityBlock activityName="rope_jumping" />
+          <ActivityBlock activityName="cycling" />
+          <ActivityBlock activityName="chess" />
+          <ActivityBlock activityName="hide_and_seek" />
+        </View>
       </View>
     </>
   );
 }
 
-function ActivitySectionParent() {
+const ProfileContent = ({ userName, isParent }) => {
   return (
-    <>
-      <View className="my-[10px] ml-[25px]">
-        <Text className="text-xl text-gray-600 font-semibold flex items-center">
-          Minhas atividades
-          <View className="ml-auto">
-            <Link href="/activities">
-              <TouchableOpacity>
-                <SetaDireita></SetaDireita>
-              </TouchableOpacity>
-            </Link>
-          </View>
-        </Text>
-        <View className=" w-4/5 lg:w-2/4 self-center h-[120vh] justify-start">
-            <View
-              style={{
-                flexGrow: 1,
-                alignItems: "center",
-                flexDirection: "column",
-                flexShrink: 1,
-              }}
-            >
-              <ActivityBlock
-                activityName="soccer"
-                ActivitySvg={Ball}
-              />
-            </View>
-          </View>
-      </View>
-    </>
-  );
-}
-
-function ParedProfile(){
-  return(
-    <View></View>
-  )
-}
-
-
-const ProfileContent = ({ userName, isParent  }) => {
-
-  return (
-    <ScrollView className="">
-      <View className="flex flex-col justify-start items-center h-[120vh]">
+    <ScrollView>
+      <View className="flex flex-col justify-start items-center h-[200vh]">
         <View>
           <View className="mb-3">
             {isParent ? (
-                <ProfileParent/>
-              ) : (
-                <ProfileChield userName={userName} />
-              )}
+              <ProfileParent />
+            ) : (
+              <ProfileChield userName={userName} />
+            )}
           </View>
 
           {isParent ? (
-                <MyDates userName={userName} dia={27}/>
-              ) : (
-                <AchievementSection></AchievementSection>
-              )}
+            <MyDates userName={userName} dia={27} />
+          ) : (
+            <AchievementSection></AchievementSection>
+          )}
 
-          
-          
-                
           <View className="my-[5px] flex justify-between">
-            {isParent ? (
-                <Text></Text>
-              ) : (
-                <GroupSection></GroupSection>
-              )}
+            {isParent ? <Text></Text> : <GroupSection></GroupSection>}
           </View>
-          
-          {isParent ? (
-                <ActivitySectionParent></ActivitySectionParent>
-              ) : (
-                <ActivitySectionChild></ActivitySectionChild>
-              )}
 
+          <ActivitySection />
         </View>
       </View>
     </ScrollView>
@@ -390,10 +318,8 @@ const ProfileContent = ({ userName, isParent  }) => {
 
 export default function Page() {
   return (
-    <View className="flex flex-1">  
-      <View className="mt-8">
-        <ProfileContent userName="Leticia" isParent={true}></ProfileContent>
-      </View>
+    <View className="flex flex-1 mt-8">
+      <ProfileContent userName="Leticia" isParent={false}></ProfileContent>
     </View>
   );
 }
