38e8815a8054a82b0b9197c9772906001936d37c
Enriqson <elaborao@hotmail.com>
2024-03-17 23:31:39 -0300

    remove index redirect to profile

--- mini-atletas/src/app/(app)/index.tsx
+++ mini-atletas/src/app/(app)/index.tsx
@@ -1,45 +1,9 @@
 import { Link, Redirect } from "expo-router";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { useAuth } from "@/providers/AuthProvider";
-import { supabase } from "@/config/supabase";
 
 export default function Page() {
-  const { user } = useAuth();
-  const [isParent, setIsParent] = useState(null);
-  const [hasDataLoaded, setHasDataLoaded] = useState(false)
-
-useEffect(() => {
-  async function fetchIsParent() {
-    let { data, error } = await supabase
-      .from("parent_child")
-      .select("child_id")
-      .eq("parent_id", user.id)
-      .maybeSingle();
-
-    if (error) {
-      console.error("Error fetching Child:", error.message);
-      return;
-    }
-
-    if (data?.child_id) {
-      setIsParent(true);
-    }
-    setHasDataLoaded(true)
-  }
-
-  fetchIsParent();
-}, [user]);
-
-
-  if(!hasDataLoaded){
-    return ""
-  }
-  if(isParent){
-    return ( <Redirect href="/profile" />)
-  }
-
   return ( <Redirect href="/adventureWeek" />)
 
   //keeping old root page
